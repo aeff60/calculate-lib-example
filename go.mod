c7dc1d8bae386aa81d88cb9b4a5f540016a97af2
Aef_SB <46419278+aeff60@users.noreply.github.com>
2023-01-16 23:36:24 +0700

    create area.go

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/aeff60/calculate-lib-example
+
+go 1.17
---
+++ pkg/shapes/area.go
@@ -0,0 +1,12 @@
+package shapes
+
+// ข้อแตกต่างระหว่าง float 32 และ float64
+func GetRectangleArea(width, height float32) float32 {
+	return width * height
+
+}
+
+func GetCircleArea(radius float32) float32
+{
+	return math.Pi * radius * radius
+}
